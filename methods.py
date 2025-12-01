1c36fd31ab94fe0706d5c58f2f3cf5660962bb9d
cedmonston22 <cedmonst@calpoly.edu>
2025-11-30 16:52:43 -0800

    Wattage Data

--- methods.py
+++ methods.py
@@ -1,30 +1,31 @@
 
 import data
-
-# NEW TEMP DATA (11/29)
-temp_data =  [data.Room(201, 100, 1,
-                         100, 10, "woozoo", 6, 24, "winix", "laptop", 3), data.Room(202, 100, 5,
-                         100, 18, "hurricane", 5, 0, "idk", "phone", 8), data.Room(205, 100, 3,
-                         100, 24, "idk", 2, 3, "blue pure", "mac", 12),
+#NEED TO INPUT ACTUAL DATA
+#TEMP DATA AS OF 11/27/2025
+temp_data =  [data.Room(201, 10000.5, 20005.7,
+                         3000.15, 200.5, 1905.67).to_dict(),
+                data.Room(202, 9701.41, 16300.59,
+                         2700.41, 676.76, 4121.25).to_dict(),
+                data.Room(204, 15090,17358.98, 22255.97,
+                         3020.47, 1000.67).to_dict()
                 ]
 
-temp_rooms = [room.to_dict() for room in temp_data]
 
 
 
-# 3 methods
+
 
 def calculate_average_all(energy_data: list[dict]):
     avg_dict = {"Shower Energy": 0, "Light Energy": 0, "Fan Energy": 0,
                 "Air Purifier Energy": 0, "Device Energy": 0}
     for room in energy_data:
-        for key in avg_dict:
-            avg_dict[key] += room[key]
+        for room_data in room:
+            if room_data in avg_dict:
+                avg_dict[room_data] += room[room_data]
 
     for key in avg_dict:
-        avg_dict[key] = round(avg_dict[key] / len(energy_data), 2)
+        avg_dict[key] = round(avg_dict[key] / len(energy_data),2)
     return avg_dict
-
 
 def find_above_average(energy_data: list[dict], energy_type: str):
     avg_data = calculate_average_all(energy_data)
@@ -38,20 +39,13 @@ def find_above_average(energy_data: list[dict], energy_type: str):
 def total_energy_average(energy_data: list[dict]):
     total_energy = 0
     for room in energy_data:
-        total_energy += room["Shower Energy"]
-        total_energy += room["Light Energy"]
-        total_energy += room["Fan Energy"]
-        total_energy += room["Air Purifier Energy"]
-        total_energy += room["Device Energy"]
-    return round(total_energy / len(energy_data), 2)
+        for room_data in room:
+            total_energy += room[room_data]
+    return round(total_energy/len(energy_data),2)
 
-def tips_for_above_average(energy_data: list[dict]):
-    pass
-# no tips yet right, i'll do this with u later
+def tips_for_above_average(energy_data: list[dict])
 
-# added extra, average total energy to main
 
 if __name__ == "__main__":
-    print(find_above_average(temp_rooms, "Shower Energy"))
-    print(find_above_average(temp_rooms, "Device Energy"))
-    print("Average Total Energy:", total_energy_average(temp_rooms))
+    print(find_above_average(temp_data, "Shower Energy"))
+    print(find_above_average(temp_data, "Device Energy"))
